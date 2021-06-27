bccaa1fabf12b3a854f34313da50638f2b7cf592
Goldy <thegoldycopythat@gmail.com>
2021-06-27 12:17:08 +0530

    initial commit

--- app/src/main/java/com/abhinav/chouhan/loaderchipdemo/MainActivity.kt
+++ app/src/main/java/com/abhinav/chouhan/loaderchipdemo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.abhinav.chouhan.loaderchipdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abhinav.chouhan.loaderchip.LoaderChip
+import com.abhinav.chouhan.loaderchipdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.activity = this
 
+    }
+
+    fun load(){
+       binding.loaderChip.startLoading()
     }
 }
--- loaderchip/src/main/java/com/abhinav/chouhan/loaderchip/LoaderChip.kt
+++ loaderchip/src/main/java/com/abhinav/chouhan/loaderchip/LoaderChip.kt
@@ -6,17 +6,26 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import android.view.animation.DecelerateInterpolator
+import android.view.animation.*
+import android.view.animation.Interpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import com.google.android.material.chip.Chip
 import kotlin.math.abs
 
 
-class LoaderChip @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null,defStyleAttr:Int = 0) : Chip(context,attributeSet) , View.OnClickListener {
+class LoaderChip @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : Chip(context, attributeSet, defStyleAttr) {
 
 
-    private lateinit var borderPath:Path
-    private lateinit var pathMeasure:PathMeasure
-    private lateinit var loaderAnimator:ValueAnimator
+    private lateinit var borderPath: Path
+    private lateinit var pathMeasure: PathMeasure
+    private lateinit var loaderAnimator: ValueAnimator
     var lapDuration = 2000L
     var loaderWidth = 5f
     var loaderColor = Color.RED
@@ -24,10 +33,14 @@ class LoaderChip @JvmOverloads constructor(context: Context, attributeSet: Attri
     private var loaderEnd = 0f
     var loaderStartColor = -1
     var loaderEndColor = -1
+    var reverseEffectEnabled = false
+    lateinit var interpolator: Interpolator
+    lateinit var reverseInterpolator: Interpolator
     var drawingGradient = false
     private var loaderPath = Path()
     private var shouldLoad = false
-    private var wrappedClickListener:OnClickListener? = null
+    private var reversed = false
+    private var wrappedClickListener: OnClickListener? = null
     var shouldStartLoadingOnClick = true
     private val loaderPaint = Paint().apply {
         isAntiAlias = true
@@ -36,9 +49,51 @@ class LoaderChip @JvmOverloads constructor(context: Context, attributeSet: Attri
 
     init {
         setEnsureMinTouchTargetSize(false)
-        super.setOnClickListener(this)
-        if(attributeSet != null) {
-            val ta = context.obtainStyledAttributes(attributeSet, R.styleable.LoaderChip, 0, 0)
+        super.setOnClickListener {
+            wrappedClickListener?.onClick(it)
+            if (shouldStartLoadingOnClick) {
+                startLoading()
+            }
+        }
+        if (attributeSet != null) {
+            val ta = context.obtainStyledAttributes(
+                attributeSet,
+                R.styleable.LoaderChip,
+                defStyleAttr,
+                0
+            )
+            reverseEffectEnabled = ta.getBoolean(R.styleable.LoaderChip_reverseEffect,false)
+            when (ta.getInt(R.styleable.LoaderChip_loaderStyle, 7)) {
+                1 -> {
+                    interpolator = FastOutSlowInInterpolator()
+                    reverseInterpolator = ReverseFastOutSlowInInterpolator()
+                }
+                2 -> {
+                    interpolator = OvershootInterpolator()
+                    reverseInterpolator = ReverseOvershootInterpolator()
+                }
+                3 -> {
+                    interpolator = BounceInterpolator()
+                    reverseInterpolator = ReverseBouceInterpolator()
+                }
+                4 -> {
+                    interpolator = AccelerateInterpolator()
+                    reverseInterpolator = ReverseAccelerateInterpolator()
+                }
+                5 -> {
+                    interpolator = DecelerateInterpolator()
+                    reverseInterpolator = ReverseDecelerateInterpolator()
+                }
+                6 -> {
+                    interpolator = LinearInterpolator()
+                    reverseInterpolator = ReverseLinearInterPolator()
+                }
+                else -> {
+                    interpolator = AccelerateDecelerateInterpolator()
+                    reverseInterpolator = ReverseAccelerateDecelrateInterPolator()
+                }
+            }
+
             val defaultLoaderWidth = resources.getDimension(R.dimen.default_loader_width)
             loaderWidth = ta.getDimension(R.styleable.LoaderChip_loaderWidth, defaultLoaderWidth)
             loaderPaint.strokeWidth = loaderWidth
@@ -57,8 +112,8 @@ class LoaderChip @JvmOverloads constructor(context: Context, attributeSet: Attri
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        with(canvas){
-            if(shouldLoad) {
+        with(canvas) {
+            if (shouldLoad) {
                 loaderPath.reset()
                 pathMeasure.getSegment(loaderStart, loaderEnd, loaderPath, true)
                 drawPath(loaderPath, loaderPaint)
@@ -70,48 +125,62 @@ class LoaderChip @JvmOverloads constructor(context: Context, attributeSet: Attri
         wrappedClickListener = l
     }
 
-    override fun onClick(v: View?) {
-        wrappedClickListener?.onClick(v)
-        if(shouldStartLoadingOnClick){
-            startLoading()
-        }
-    }
 
-    fun stopLoading(){
+    fun stopLoading() {
         shouldLoad = false
         loaderAnimator.end()
     }
 
 
-    fun startLoading(){
+    fun startLoading() {
         shouldLoad = true
         borderPath = Path().apply {
-            addRoundRect(0f + loaderPaint.strokeWidth/2,
-                0f + loaderPaint.strokeWidth/2,
-                width.toFloat() - loaderPaint.strokeWidth/2,
-                height.toFloat() - loaderPaint.strokeWidth/2,
-                if(chipCornerRadius == 0f) height/2f else chipCornerRadius,
-                if(chipCornerRadius == 0f) height/2f else chipCornerRadius,
-                Path.Direction.CCW)
+            addRoundRect(
+                0f + loaderPaint.strokeWidth / 2,
+                0f + loaderPaint.strokeWidth / 2,
+                width.toFloat() - loaderPaint.strokeWidth / 2,
+                height.toFloat() - loaderPaint.strokeWidth / 2,
+                if (chipCornerRadius == 0f) height / 2f else chipCornerRadius,
+                if (chipCornerRadius == 0f) height / 2f else chipCornerRadius,
+                Path.Direction.CCW
+            )
         }
 
-        pathMeasure = PathMeasure(borderPath,true)
 
-        if(drawingGradient){
-            loaderPaint.shader = LinearGradient(0f,0f,pathMeasure.length/3f,loaderWidth,Color.RED,Color.YELLOW,Shader.TileMode.CLAMP)
-        }else{
+        pathMeasure = PathMeasure(borderPath, true)
+
+        if (drawingGradient) {
+            loaderPaint.shader = LinearGradient(
+                0f,
+                0f,
+                pathMeasure.length / 3f,
+                loaderWidth,
+                loaderStartColor,
+                loaderEndColor,
+                Shader.TileMode.CLAMP
+            )
+        } else {
             loaderPaint.color = loaderColor
         }
-        loaderAnimator = ValueAnimator.ofFloat(0f,1f).apply {
-            duration =  lapDuration
-            repeatCount = ValueAnimator.INFINITE
-            interpolator = DecelerateInterpolator()
+        loaderAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
+            duration = lapDuration
+            this.interpolator = this@LoaderChip.interpolator
             addUpdateListener {
                 val animatedValue = it.animatedValue as Float
-                loaderEnd  = animatedValue * pathMeasure.length
+                loaderEnd = animatedValue * pathMeasure.length
                 loaderStart = loaderEnd - ((0.5f - abs(animatedValue - 0.5f)) * pathMeasure.length)
                 invalidate()
             }
+        }
+        loaderAnimator.doOnStart {
+            reversed = !reversed
+        }
+        loaderAnimator.doOnEnd {
+            if(reverseEffectEnabled) {
+                loaderAnimator.interpolator =
+                    if (reversed) interpolator else reverseInterpolator
+            }
+            loaderAnimator.start()
         }
         loaderAnimator.start()
     }
